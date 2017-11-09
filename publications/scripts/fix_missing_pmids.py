ba539f37100eb3420db40371d989a8f9e8843e4c
Per Kraulis <per.kraulis@scilifelab.se>
2017-11-09 14:23:07 +0100

    Added jump to fix_missing_pmids.py

--- publications/scripts/fix_missing_pmids.py
+++ publications/scripts/fix_missing_pmids.py
@@ -9,11 +9,12 @@ from publications import utils
 
 DELAY = 2.0
 
-def fix_missing_pmids(db):
+def fix_missing_pmids(db, jump=0):
     view = db.view('publication/no_pmid', include_docs=True)
-    for item in view:
+    for pos, item in enumerate(view):
+        if pos < jump: continue
         title = item.doc['title']
-        print(title)
+        print("[%s]" % pos, title)
         pmids = pubmed.search(title=title, delay=DELAY)
         if len(pmids) == 1:
             with PublicationSaver(doc=item.doc, db=db) as saver:
@@ -24,6 +25,9 @@ def fix_missing_pmids(db):
 def get_args():
     parser = utils.get_command_line_parser(
         'Fx missing PMID in all publications by searching for title.')
+    parser.add_argument('-j', '--jump',
+                        action='store', dest='jump', type=int, default=0,
+                        help='jump over the first number of publications')
     return parser.parse_args()
 
 
@@ -31,4 +35,4 @@ if __name__ == '__main__':
     args = get_args()
     utils.load_settings(filepath=args.settings)
     db = utils.get_db()
-    fix_missing_pmids(db)
+    fix_missing_pmids(db, jump=args.jump)
