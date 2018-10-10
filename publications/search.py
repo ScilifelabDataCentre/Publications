9e59c428342e4708eaca290d592b2a60b5656291
Per Kraulis <per.kraulis@gmail.com>
2018-10-10 22:21:48 +0200

    Simplified search.

--- publications/search.py
+++ publications/search.py
@@ -58,14 +58,14 @@ class Search(RequestHandler):
             terms = [utils.strip_prefix(t)
                      for t in self.get_argument('terms', '').split()]
             terms = [t.lower() for t in terms if t]
-        hits = {}
+        iuids = set()
         for viewname in [None,
                          'publication/doi',
                          'publication/published',
                          'publication/epublished',
                          'publication/issn',
                          'publication/journal']:
-            self.search(viewname, terms, hits)
+            iuids.update(self.search(viewname, terms))
         # Now remove set of insignificant characters.
         terms = [''.join([c for c in t if c not in REMOVE])
                  for t in terms]
@@ -75,29 +75,28 @@ class Search(RequestHandler):
                          'publication/notes',
                          'publication/pmid',
                          'publication/label_parts']:
-            self.search(viewname, terms, hits)
-        publications = [self.get_publication(id) for id in hits]
-        for publication in publications:
-            publication['$score'] = (hits[publication['_id']],
-                                     publication['published'])
-        publications.sort(key=lambda p: p['$score'], reverse=True)
+            iuids.update(self.search(viewname, terms))
+        publications = [self.get_publication(iuid) for iuid in iuids]
+        publications.sort(key=lambda p: p['published'], reverse=True)
         self.render('search.html',
                     publications=publications,
                     terms=self.get_argument('terms', ''))
 
-    def search(self, viewname, terms, hits):
-        "Search the given view using the terms"
+    def search(self, viewname, terms):
+        "Search the given view using the terms. Return set of IUIDs."
+        result = set()
         if viewname is None:
             # IUID of publicaton entry.
             for term in terms:
                 if term in self.db:
-                    hits[term] = hits.get(term, 0) + 1
+                    result.add(term)
         else:
             view = self.db.view(viewname, reduce=False)
             for term in terms:
                 if term in IGNORE: continue
                 for item in view[term : term + constants.CEILING]:
-                    hits[item.id] = hits.get(item.id, 0) + 1
+                    result.add(item.id)
+        return result
 
 
 class SearchJson(Search):
